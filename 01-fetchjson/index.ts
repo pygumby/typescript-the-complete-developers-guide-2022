494892707d2ece0065baf55b0a4e228b7ff892ef
Lucas Konstantin Bärenfänger <18075314+lkbaerenfaenger@users.noreply.github.com>
2022-06-16 13:50:51 +0200

    Add section-01-lecture-08

--- 01-fetchjson/index.ts
+++ 01-fetchjson/index.ts
@@ -3,5 +3,15 @@ import axios from 'axios'
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 axios.get(url).then(response => {
-  console.log(response.data)
+  const todo = response.data
+
+  const id = todo.ID
+  const title = todo.Title
+  const finished = todo.finished
+
+  console.log(`
+    ID: ${id}
+    Title: ${title}
+    Finished: ${finished}
+  `)
 })
