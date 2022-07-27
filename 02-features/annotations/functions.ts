5f4188fed2846d3a896ae86a83539453c43bfd02
Lucas Konstantin Bärenfänger <18075314+lkbaerenfaenger@users.noreply.github.com>
2022-07-27 20:19:34 +0200

    Add section-04-lecture-29

--- 02-features/annotations/functions.ts
+++ 02-features/annotations/functions.ts
@@ -21,3 +21,21 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message)
 }
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+}
+
+// ES2015
+// const logWeather = ({ date, weather }) => {
+//   console.log(date)
+//   console.log(weather)
+// }
+
+const logWeather = ({date, weather}: { date: Date, weather: string }): void => {
+  console.log(date)
+  console.log(weather)
+}
+
+logWeather(todaysWeather)
