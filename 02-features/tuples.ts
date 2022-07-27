f611848e64206450f0adeefb28a9c6718fb92679
Lucas Konstantin Bärenfänger <18075314+lkbaerenfaenger@users.noreply.github.com>
2022-07-27 22:53:12 +0200

    Add section-06-lecture-37

--- 02-features/tuples.ts
+++ 02-features/tuples.ts
@@ -9,3 +9,10 @@ type Drink = [string, boolean, number]
 const pepsi: Drink = ['brown', true, 40]
 const sprite: Drink = ['clear', true, 40]
 const tea: Drink = ['brown', false, 0]
+
+// Compare the following two values to see why tuples are not that useful:
+const carSpecs: [number, number] = [400, 3354]
+const carStats = {
+  horsePower: 400,
+  weight: 3354,
+}
