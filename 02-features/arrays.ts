b573322cfc23b20165a0a157d225667798308470
Lucas Konstantin Bärenfänger <18075314+lkbaerenfaenger@users.noreply.github.com>
2022-07-27 21:33:46 +0200

    Add section-05-lecture-32

--- 02-features/arrays.ts
+++ 02-features/arrays.ts
@@ -1,4 +1,16 @@
-const carMakers = ['ford', 'toyota', 'chevy']
+const carMakers = ['Ford', 'Toyota', 'Chevy']
 const dates = [new Date(), new Date()]
 
 const carsByMake: string[][] = []
+
+// Help with inference when extracting values
+const car = carMakers[0]
+const myCar = carMakers.pop()
+
+// Prevent incompatible values
+// carMakers.push(100)
+
+// Help with 'map'
+carMakers.map((carMaker: string): string => {
+  return carMaker
+})
